7465ee25adb98ffe0b2cf3d1cd236896ef5d18d9
4arjun <arjunajith440@gmail.com>
2025-03-07 18:18:13 +0530

    vercel config

---
+++ public/sw.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('your-app-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        // Add other assets you want to cache
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+}); 
