43beb2f72cdbd73df9560a0ba16317903b94c12f
stefanbinoj <stefan.binoj.007@gmail.com>
2025-03-07 14:32:47 +0530

    reoved navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Navbar from "./components/Navbar"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import "./App.css"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import "./App.css";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto p-4 t">
       <h1 className="text-3xl font-bold">Home Page</h1>
       <p className="mt-4">Welcome to our website!</p>
     </div>
-  )
+  );
 }
 
-export default Home 
+export default Home;
